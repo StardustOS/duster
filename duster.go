07a43d18f7aa0828307b7050140902fbef0b29e7
Kareem <mka@st-andrews.ac.uk>
2020-04-22 11:33:24 +0000

    Added some more validation to the input

--- duster.go
+++ duster.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"compress/gzip"
+	"encoding/binary"
 	"flag"
 	"fmt"
-	"compress/gzip"
 	"io/ioutil"
-	"strings"
 	"os"
-	"encoding/binary"
+	"os/user"
+	"strings"
 
 	"github.com/AtomicMalloc/debugger/cli"
 	"github.com/AtomicMalloc/debugger/debugger"
-	"github.com/AtomicMalloc/debugger/xen"
 	"github.com/AtomicMalloc/debugger/file"
+	"github.com/AtomicMalloc/debugger/xen"
 )
 
 func unzipFile(filename string) string {
@@ -39,12 +40,24 @@ func unzipFile(filename string) string {
 }
 
 func main() {
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// 0 means we're running as root (annoyingly go treats this has a string)
+	if strings.Compare(currentUser.Uid, "0") != 0 {
+		fmt.Println("Error: debugger must run as root (so it can interface with the Xen API)")
+		os.Exit(1)
+	}
+
 	cmd := cli.CLI{}
 
 	var id int
 	var filename string
-	flag.StringVar(&filename, "path", "", "Path to the os's binary")
-	flag.IntVar(&id, "id", -1, "The domain id to connect")
+	flag.StringVar(&filename, "path", "", "Path to the Operating System's binary")
+	flag.IntVar(&id, "id", 0, "The domain id to connect (can be found by running sudo xl list)")
 	flag.Parse()
 
 	if id == -1 {
@@ -55,17 +68,42 @@ func main() {
 		os.Exit(1)
 	}
 
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Printf("Error: %s does not exist\n", filename)
+		os.Exit(1)
+	}
+
 	domainid := uint32(id)
 
 	cntrl := &xen.Xenctrl{DomainID: domainid}
-	cntrl.Init()
-	cntrl.SetDebug(domainid, true)
-	
+	err = cntrl.Init()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = cntrl.SetDebug(domainid, true)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("The above error is most likely caused by the domain id not existing")
+		os.Exit(1)
+	}
+
 	mem := &xen.Memory{Domainid: domainid, Vcpu: 0}
-	mem.Init(cntrl)
+	err = mem.Init(cntrl)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	filename = unzipFile(filename)
-	
+
 	p, err := file.NewSymbolicInformation(filename, binary.LittleEndian)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	f := &file.LineInformation{Name: filename}
 	err = f.Init()
 	if err != nil {
@@ -74,7 +112,6 @@ func main() {
 	}
 	dbg := debugger.NewDebugger(mem, cntrl, f, cntrl, p)
 	cmd.Init(dbg)
-	
 
 	fmt.Println("Welcome to Duster!")
 	for {

8c302c4db2baf161f172b851a182fccd1260e26b
Kareem <mka@st-andrews.ac.uk>
2020-04-27 15:11:21 +0000

    added dependency install script

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+add-apt-repository ppa:longsleep/golang-backports
+apt update
+apt install golang-go
+apt-get install xen-hypervisor-4.9-amd64 xen-system-amd64 xen-tools xen-utils-4.9 xen-utils-common xenstore-utils xenwatch
+apt-get install libxen-dev
